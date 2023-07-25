d7ff28ba98c6f71f86fd383fb766f0f47156a52d
ryuw1 <fun@sousaikun.com>
2023-07-25 21:58:26 +0900

    20230725 21:49 adJapaneseCalenderCompatibilityのテストダブルを追加。

--- routes/webTemplate.php
+++ routes/webTemplate.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 Route::prefix('/mastas/*/')->group(function () {
     Route::get('/', [`Controller::class, 'index'])->name('*.index');
     Route::get('/create', [`Controller::class,  'create'])->name('*.create');
@@ -6,4 +7,14 @@ Route::prefix('/mastas/*/')->group(function () {
     Route::get('/edit/{id}', [`Controller::class,  'edit'])->name('*.edit');
     Route::post('/update/{id}', [`Controller::class,  'update'])->name('*.update');
     Route::get('/destoy/{id}', [`Controller::class, 'destroy'])->name('*.destroy');
+=======
+Route::prefix('/mastas/unit/')->group(function () {
+    Route::get('/', [UnitController::class, 'index'])->name('unit.index');
+    Route::get('/create', [UnitController::class,  'create'])->name('unit.create');
+    //Route::post('/create', [UnitController::class, 'create'])->name('unit.created');
+    Route::post('/store', [UnitController::class, 'store'])->name('unit.store');
+    Route::get('/edit/{id}', [UnitController::class,  'edit'])->name('unit.edit');
+    Route::post('/update/{id}', [UnitController::class,  'update'])->name('unit.update');
+    Route::get('/destoy/{id}', [UnitController::class, 'destroy'])->name('unit.destroy');
+>>>>>>> fe2c076 (20230725 21:49)
 });
